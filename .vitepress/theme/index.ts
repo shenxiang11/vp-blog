11c0bedce595b2d372e538949e6a5f249e2c2053
shenxiang11 <shenxiang11@users.noreply.github.com>
2023-05-10 13:15:55 +0000

    deploy: 3ead88ac8422d721839a2ec56be99ba870797b17

--- .vitepress/theme/index.ts
+++
@@ -1,9 +0,0 @@
-// https://vitepress.dev/guide/custom-theme
-import Theme from 'vitepress/theme';
-import TheLayput from '@/TheLayout.vue'
-
-export default {
-  ...Theme,
-  Layout: TheLayput,
-}
-
