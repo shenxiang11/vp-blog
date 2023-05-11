fe130be5126e57728fc7fb269f2e22065bb20f7e
Shen <863461783@qq.com>
2023-05-11 21:52:24 +0800

    feature:create post command

---
+++ createPost.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const fs = require('node:fs/promises')
+const dayjs = require("dayjs");
+
+!(async function() {
+  const dir = path.resolve(__dirname, process.argv[2])
+  const filename = process.argv[3] + '.md'
+
+  let result = await fs.access(dir, fs.constants.W_OK)
+
+  await fs.writeFile(`${dir}/${filename}`, `---
+title: ${process.argv[3]}
+date: ${dayjs().format('YYYY-MM-DD HH:mm:ss')}
+tags: []
+layout: post
+cover:
+  image: ***
+---
+
+  `)
+})()
+
+
+
+
+
