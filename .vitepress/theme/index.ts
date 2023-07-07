87e0a3f40662aa49132a5f245c32e1289eeb2b81
Shen <863461783@qq.com>
2023-07-07 10:45:16 +0800

    fix: fix name

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,9 +1,8 @@
 // https://vitepress.dev/guide/custom-theme
-import Theme from 'vitepress/theme';
-import TheLayput from '@/TheLayout.vue'
+import Theme from 'vitepress/theme'
+import TheLayout from '@/TheLayout.vue'
 
 export default {
   ...Theme,
-  Layout: TheLayput,
+  Layout: TheLayout
 }
-
